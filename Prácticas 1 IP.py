284c78dc60e755c2c66d538b734d0f012ca3d25b
3evian <111407651+3evian@users.noreply.github.com>
2022-08-17 00:46:13 -0300

    Add files via upload

---
+++ Prácticas 1 IP.py
@@ -0,0 +1,54 @@
+#numero1=int(input("ingresar valor"))
+
+#numero2=int(input("ingresar valor2"))
+
+#print(numero1 + numero2)
+
+#valor=int(input("ingresar valor 3"))
+
+#print(valor+1)
+
+#print((6+5)/2)
+
+#nota1=int(input("ingresar valor"))
+
+#nota2=int(input("ingresar valor"))
+
+#print((nota1+nota2)/2)
+
+#print("Mi primer programa Python")
+
+#print("V\nE\nR\nT\nI\nC\nA\nL")
+
+#print("*****\n*   *\n*   *\n*   *\n*****")
+
+#print("*\n***\n*****\n*******\n*********")
+
+#opinion=input("ingrese opinión")
+
+#print(opinion)
+
+#valor=input("ingrese valoración")
+
+#print("*******************************")
+#print("* El resultado es " + valor)
+#print("*******************************")
+
+#capital=int(input("ingresar monto"))
+#meses=int(input("tiempo"))
+#interes=int(input("interes"))
+
+#interestotal=((capital*meses*interes)/100)
+
+#print(capital+interestotal)
+
+#costo=float(input("costo"))
+
+#adicional=float(input("adicional"))
+
+#llamadas=float(input("llamadas"))
+
+#tiempo=float(input("tiempo"))
+
+#print((costo*llamadas)+adicional*tiempo)
+
