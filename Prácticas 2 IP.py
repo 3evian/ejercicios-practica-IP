c679c382208d8c51a26ded3c19cfbcd54fb327d7
3evian <111407651+3evian@users.noreply.github.com>
2022-08-24 01:13:01 -0300

    Add files via upload

---
+++ Prácticas 2 IP.py
@@ -0,0 +1,110 @@
+#edad=int(input("edad"))
+#if(edad>70):
+#    print("excento")
+#else:
+#    print("vota")
+#distancia=int(input("distancia"))
+#if(distancia>500):
+#    print("excento")
+#else:
+#    print("vota")
+
+#EJERCICIO 3
+
+#a=10
+#if a>0:
+#    print("te quiero")
+#print("bien lejos")
+
+a=5
+b=3
+c=2
+
+#if a<b*c:
+#    print("Hola!")
+#else:
+#    print("Chau!")
+
+#p1=3.14
+#
+#p2=3.141569
+#
+#if p1==p2:
+#    print("son iguales")
+#else:
+#    print("no son iguales")
+
+#EJERCICIO 4
+
+#a ="Hola"
+#b ="hola"
+#if a == b :
+#    print("Python es insensible!")
+#else:
+#    print("Python es muy sensible!")
+
+#EJERCICIO 5
+
+#nota=int(input("nota del alumno"))
+#if nota<4:
+#    print("debe recuperar")
+#else:
+#    print ("se salvó")
+
+#EJERCICIO 6
+
+#valor1=int(input("valor1"))
+#valor2=int(input("valor2"))
+#if valor1>valor2:
+#    print("valor1 es mayor")
+#else:
+#    print("valor2 es mayor")
+
+#EJERCICIO 7
+
+#nota1=float(input("ingrese nota "))
+#nota2=float(input("ingrese nota "))
+####me falta hacer promedio
+
+#EJERCICIO 8
+
+#numero=int(input("ingrese un numero"))
+#if numero<10:
+#    print("numero de una cirfra")
+#if numero>9:
+#    print("numero con mas de una cifra")
+
+#EJERCICIO 9
+
+#30612453
+#23763290
+#21448503
+#34582048
+#15364857
+#43261864
+#documentosconocidos=a+b+c+d+e
+#documento=int(input("numero de documento"))
+#if documento==documentosconocidos:
+#    print("existe")
+#else:
+#    print("desconocido")
+
+#EJERCICIO 10
+
+#fijo=480
+#exceso=250*25.5
+#impuesto=78
+
+#print(("total a pagar",fijo+exceso+impuesto))
+
+#EJERCICIO 11
+
+numero1=int(input("primer numero"))
+numero2=int(input("segundo numero"))
+numero3=int(input("tercer numero"))
+if numero1>numero2 and numero3:
+    print("numero1 es el mayor")
+if numero2>numero1 and numero3:
+    print("numero2 es el mayor")
+if numero3>numero1 and numero2:
+    print("numero3 es el mayor")
